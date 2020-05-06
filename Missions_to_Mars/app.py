d24e8ae1dbdec785fb2f1de5f38a1da3d52b19b7
leahlindy <58431732+leahlindy@users.noreply.github.com>
2020-05-06 11:45:17 -0400

    app test

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -2,21 +2,39 @@
 
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_craigslist
+import scrape_mars
 
+# Create an instance of Flask
 app = Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
-mongo = PyMongo(app)
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
-# Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
+# create route that renders index.html template
+@app.route("/")
+def index():
+    # Find a data point from mars collection and save in mars variable
+    mars = mongo.db.mars.find_one()
+     # Return template and data (mars- in index.html)
+    return render_template("index.html", mars=mars)
+
 
 # define route for scrape to import scrape_mars.py
-@app.route("scrape/")
+@app.route("/scrape")
 def scraper():
+    #db is called mars
     mars = mongo.db.mars
+
+    #create variable to hold data form scrape_mars.py with scrape_sites function applied 
     mars_data = scrape_mars.scrape_sites()
-    mars.update({}, mars_data, upsert=True)
+    #update db with dictionary holding the mars_data
+    mars.update(
+        {}, 
+        mars_data, 
+        upsert=True)
+    
+    # Redirect to home page
     return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
