247a1cbc8ae6ddf42a7629bd4b6346f5ece388b2
leahlindy <58431732+leahlindy@users.noreply.github.com>
2020-05-06 11:04:23 -0400

    app.py file

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,17 @@
+# Use MongoDB with Flask templating to create a new HTML page that displays all of the information that was scraped from the URLs.
+
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_craigslist
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
+mongo = PyMongo(app)
+
+# Or set inline
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
+
+# define route for scrape to import scrape_mars.py
+@app.route("scrape/")
